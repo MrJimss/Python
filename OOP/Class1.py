268c37bc1c9540e9738cd6723e1292af53a165ce
cmbarros <carlosmbarros@icloud.com>
2024-04-17 08:25:53 -0700

    add new method for class testing in class1

--- OOP/Class1.py
+++ OOP/Class1.py
@@ -1,3 +1,10 @@
 class Dog:
      def __init__ (self):
           self.name= "lassie"
+     def bark(self,name):
+          print(f"woof, my name is {name}")
+          
+          
+dog = Dog()
+print(dog.name)
+dog.bark("perro")
